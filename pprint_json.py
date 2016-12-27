3e993505dcc24499711923bc0681b8066e07c2aa
xDSUP <ilya.elfimow@yandex.ru>
2016-12-27 19:10:54 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,13 @@ import json
 
 
 def load_data(filepath):
-    pass
-
+    with open(filepath, 'r') as file_handler:
+        return json.load(file_handler)
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input('input filepath:   ')
+    pretty_print_json(load_data(filepath))
